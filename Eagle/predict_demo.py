3006abc9c8b744e128d5011f63f491ae5e27131a
Guangxin_Mi <u7802794@anu.edu.au>
2025-08-30 20:01:07 +1000

    updated

--- Eagle/predict_demo.py
+++ Eagle/predict_demo.py
@@ -33,7 +33,7 @@ import argparse
 from transformers import TextIteratorStreamer
 from threading import Thread
 
-model_path = "NVEagle/Eagle-X5-7B-Chat"
+model_path = "NVEagle/Eagle-X4-7B-Chat"
 conv_mode = "vicuna_v1"
 image_path = "assets/georgia-tech.jpeg"
 input_prompt = "Describe this image."
